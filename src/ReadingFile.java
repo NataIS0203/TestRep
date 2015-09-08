3436d751f1bf286e778fc0ca8f1fd343ae283f38
Natalya Sniff <natalyasniff@gmail.com>
2015-09-08 13:27:08 -0400

    Trying to get file read and string adjusted

--- src/ReadingFile.java
+++ src/ReadingFile.java
@@ -1,12 +1,8 @@
 import java.lang.String;
-import java.io.*;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class ReadingFile {
 
